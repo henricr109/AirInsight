c9a52cef33acbddccb1afdd51063882dbd043a0b
NailliwFerreir <williansferreir@icloud.com>
2023-03-20 11:17:53 -0300

    Conexão bd

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
-import json
 from fastapi.middleware.cors import CORSMiddleware
+import json
+from pydantic import BaseModel
+
+class Samples(BaseModel):
+    id:int 
+    so2:int
+    co:int
+    mp10:int
+    mp25:int
+    no2:int 
+    o3:int
+
 
 app = FastAPI()
 
@@ -14,6 +25,103 @@ app.add_middleware(
 )
 
 @app.get("/samples")
-def home():
+async def home():
     dataSamples = open('data.json')
     return json.load(dataSamples)
+def createMessage(X):
+    if X == 1:
+        return "Qualidade do ar boa"
+    elif X == 2:
+        return "Qualidade do ar moderada"
+    elif X == 3:
+        return "Qualidade do ar ruim"
+    elif X == 4:
+        return "Qualidade do ar muito ruim"
+    elif X == 5:
+        return "Qualidade do ar pessima"
+    
+def calcularPureza (mp10, mp25, o3, co, no2, so2):
+    if mp10 <= 50:
+        qualidadeMp10 = 1
+    elif 50 < mp10 <= 100:
+        qualidadeMp10 = 2
+    elif 100 < mp10 <= 150:
+        qualidadeMp10 = 3
+    elif 150 < mp10 <= 250:
+        qualidadeMp10 = 4
+    elif  mp10 > 250:
+       qualidadeMp10 = 5
+
+    if mp25 <= 25:
+        qualidadeMp25 = 1
+    elif 25 < mp25 <= 50:
+        qualidadeMp25 = 2
+    elif 50 < mp25 <= 75:
+        qualidadeMp25 = 3
+    elif 75 < mp25 <= 125:
+        qualidadeMp25 = 4
+    elif  mp25 > 125:
+       qualidadeMp25 = 5
+
+    if o3 <= 100:
+        qualidadeo3 = 1
+    elif 100 < o3 <= 130:
+        qualidadeo3 = 2
+    elif 130 < o3 <= 160:
+        qualidadeo3 = 3
+    elif 160 < o3 <= 200:
+        qualidadeo3 = 4
+    elif  o3 > 200:
+       qualidadeo3 = 5
+
+    if co <= 9:
+        qualidadeco = 1
+    elif 9 < co <= 11:
+        qualidadeco = 2
+    elif 11 < co <= 13:
+        qualidadeco = 3
+    elif 13 < co <= 15:
+        qualidadeco = 4
+    elif  co > 15:
+       qualidadeco = 5
+
+    if no2 <= 200:
+        qualidadeno2 = 1
+    elif 200 < no2 <= 240:
+        qualidadeno2 = 2
+    elif 240 < no2 <= 320:
+        qualidadeno2 = 3
+    elif 320 < no2 <= 1130:
+        qualidadeno2 = 4
+    elif  no2 > 1130:
+       qualidadeno2 = 5
+    
+    if so2 <= 20:
+        qualidadeso2 = 1
+    elif 20 < so2 <= 40:
+        qualidadeso2 = 2
+    elif 40 < so2 <= 365:
+        qualidadeso2 = 3
+    elif 365 < so2 <= 800:
+        qualidadeso2 = 4
+    elif  so2 > 800:
+       qualidadeso2 = 5
+    
+    List = [qualidadeMp10, qualidadeMp25, qualidadeo3, qualidadeco,qualidadeno2,qualidadeso2]
+    qualidade = 0
+    for i in range(0,6):
+        if List[i] > qualidade:
+            qualidade = List[i]
+    return qualidade
+        
+@app.post("/samples")
+async def calcPurity(amostra:Samples):
+    res = calcularPureza(amostra.mp10,amostra.mp25,amostra.o3,amostra.co,amostra.no2,amostra.so2)
+    res = createMessage(res)
+    return json.dumps({"mensagem":f"Resposta: {res}"})
+
+@app.post("/teste")
+def testeCalc(num):
+    
+    return num
+
--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -1,31 +1,52 @@
-/*function sendSamples() {
-    let pm25 = document.getElementById('pm25').value,
-    pm10 = document.getElementById('pm10').value,
-    co = document.getElementById('co').value,
-    so2 = document.getElementById('so2').value,
-    o3 = document.getElementById('o3').value,
-    no2 = document.getElementById('no2').value;
-    const samples = {
-        pm25,
-        pm10,
-        co,
-        so2,
-        o3,
-        no2
-    }
-    JSON.stringify(samples);
-    fetch(`http://127.0.0.1:8000/samples/${samples}`,"POST")
-    .then((response)=> {
-        console.log(response)
-    })
-    .catch(
-        console.log('não recebeu o backend')
-    )
-};*/
 
+const url = "http://localhost:8000"
 async function showSamples() {
-    await fetch("http://localhost:8000/samples")
+    const tbody = document.querySelector("tbody")
+    await fetch(`${url}/samples`)
     .then((res)=> res.json())
-    .then((json)=> console.log(json))
+    .then((body)=> {
+        for (let s in body.samples){
+            let newTR = document.createElement('tr')
+            newTR.innerHTML = `<th scope="row">${body.samples[s].id}</th><td>${body.samples[s].co}</td><td>${body.samples[s].so2}</td><td>${body.samples[s].no2}</td><td>${body.samples[s].o3}</td><td>${body.samples[s].mp25}</td><td>${body.samples[s].mp10}</td>`;
+            tbody.appendChild(newTR);
+        }
+    })
     .catch(console.error());
 }
+async function sendSamples() {
+    let amostras = {
+        id:1,
+        so2:12,
+        co:24,
+        mp10:45,
+        mp25:32,
+        no2:56,
+        o3:21   
+    }
+    //amostras = JSON.stringify(amostras)
+    console.log(amostras)
+    await axios.post(`http://localhost:8000/samples`,{
+        body:amostras,
+        headers: {
+          'Content-Type': 'application/json'
+        }
+    })
+    .then((response)=> response.json())
+    .then((json)=> console.log(json))
+    .catch((error)=> console.log(`Error is : ${error}`))
+}
+
+const testeFunc = async () => {
+    await axios.post(`http://localhost:8000/teste`,{
+    body: `{ "num": 1}`,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+})
+.then((response)=> response.json())
+.then((json)=> console.log(json))
+.catch((error)=> console.log(`Error is : ${error}`))
+}
+
+
+
